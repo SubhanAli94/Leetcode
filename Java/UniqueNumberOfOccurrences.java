de8c8725d804da8cc2e2e5eaf515921140e6dede
Subhan Ali <subhanali.softengg@gmail.com>
2019-09-29 07:41:59 +0400

    Unique Number of Occurrences in Array

---
+++ Java/UniqueNumberOfOccurrences.java
@@ -0,0 +1,33 @@
+package com.company.contest156;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class UniqueNumberOfOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        int length = arr.length;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < length; i++) {
+
+            if (map.containsKey(arr[i])) {
+                //increment value
+                int value = map.get(arr[i]);
+                map.put(arr[i], ++value);
+            } else {
+                //add key with value 1
+                map.put(arr[i], 1);
+            }
+        }
+
+        List<Integer> list = new ArrayList<>(map.values());
+        list.sort(null);
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) == list.get(i+1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
