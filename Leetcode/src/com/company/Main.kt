7d6b37ff7094c54bed8a9c88e51c09219aa4f49b
Subhan Ali <subhanali.softengg@gmail.com>
2019-11-17 22:19:00 +0400

    Restructure

---
+++ Leetcode/src/com/company/Main.kt
@@ -0,0 +1,36 @@
+package com.company
+
+import com.company.problems.ctci.ArraysAndStrings.IsPermutation
+import com.company.problems.ctci.ArraysAndStrings.URLify
+import com.company.problems.ctci.ArraysAndStrings.UniqueCharacters
+
+
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        //a, a -> true
+        //a, ab -> false
+        //a*, ab -> false
+        //a*, aa -> true
+        //a*b, ab -> true
+        //a*b*c, aabc -> true
+        //.* , aabc -> true
+
+
+
+        println(URLify.urlify("abc d b ".toCharArray(),8))
+        //        System.out.println(StringPermutations.Companion.checkInclusion("aapple", "123dw"));
+        //        System.out.println(RegularExpressionMatching.Companion.isMatch("a","a"));
+        //        System.out.println(RegularExpressionMatching.Companion.isMatch("a","ab"));
+        //        System.out.println(RegularExpressionMatching.Companion.isMatch("ab","a*"));
+        //        System.out.println(RegularExpressionMatching.Companion.isMatch("aa","a*"));
+        //        System.out.println(RegularExpressionMatching.Companion.isMatch("ab","a*b"));
+        //        System.out.println(RegularExpressionMatching.Companion.isMatch("aabc","a*b*c"));
+        //        System.out.println(RegularExpressionMatching.Companion.isMatch("aabc",".*"));
+        //        System.out.println(RegularExpressionMatching.Companion.isMatch("mississippi","mis*is*p*."));
+        //        System.out.println(RegularExpressionMatching.Companion.isMatch("acd",".*"));
+        //        System.out.println(RegularExpressionMatching.Companion.isMatch("aaaa","ab*a*c*a"));
+    }
+}
