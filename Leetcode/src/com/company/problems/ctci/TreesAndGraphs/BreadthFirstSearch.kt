dd8a59c921e6d1192798be364b02806921aff17d
Subhan Ali <subhanali.softengg@gmail.com>
2020-01-10 17:15:04 +0400

    Refactoring

--- Leetcode/src/com/company/problems/ctci/TreesAndGraphs/BreadthFirstSearch.kt
+++ Leetcode/src/com/company/problems/ctci/TreesAndGraphs/BreadthFirstSearch.kt
@@ -4,14 +4,14 @@ import java.util.*
 
 class BreadthFirstSearch {
     fun searchFriend(personName: String, root: GraphNode) {
-        if (personName == root.name) print("Found Here")
+        if (personName == root.name) print("Found!!")
         var queue: Queue<GraphNode> = LinkedList<GraphNode>()
         root.visited = true
         queue.add(root)
         while (!queue.isEmpty()) {
             var node = queue.remove()
             if (node.name == personName) {
-                print("Found Here")
+                print("Found!!")
             } else {
                 var l = node.neighbours.filter { !it.visited }
                 l.forEach {
